aad5e469acc918ee10b1e18268951ea95fce4fe1
student Azhar khan <student azruddinkhan952062@gmail.com>
2026-02-17 20:23:33 +0530

    final cors fix working

--- index.js
+++ index.js
@@ -25,10 +25,12 @@ connectCloudinary();
 // middleware;
 
 app.use(express.json());
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
-app.options("/*", cors({
+
+app.use(cors({
   origin: process.env.CLIENT_URL,
-  credentials: true
+  credentials: true,
+  methods: ["GET","POST","PUT","DELETE","PATCH","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"]
 }));
 
 app.use(cookieParser());
