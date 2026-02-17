4149d1ff94478806ddb1644d3ea67cb495712039
student Azhar khan <student azruddinkhan952062@gmail.com>
2026-02-17 23:36:23 +0530

    cors fix

--- index.js
+++ index.js
@@ -22,10 +22,9 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: process.env.CLIENT_URL,
+    origin: true,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    
 }));
 
 app.use(cookieParser());
