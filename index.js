f595c628e35a7d167429229fa3454158612640f3
student Azhar khan <student azruddinkhan952062@gmail.com>
2026-02-17 20:15:27 +0530

    fix cors wildcard

--- index.js
+++ index.js
@@ -26,10 +26,11 @@ connectCloudinary();
 
 app.use(express.json());
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
-app.options("*", cors({
+app.options("/*", cors({
   origin: process.env.CLIENT_URL,
   credentials: true
 }));
+
 app.use(cookieParser());
 
 //Api Endpoints;
